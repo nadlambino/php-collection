6a99668648563953fc5f62315b042661be7f1d48
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2023-12-30 19:12:31 +0800

    feat: add basic collection methods

---
+++ Contracts/CollectionInterface.php
@@ -0,0 +1,161 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inspira\Collection\Contracts;
+
+use ArrayAccess;
+use Closure;
+use Countable;
+use Inspira\Contracts\Arrayable;
+use IteratorAggregate;
+use Traversable;
+
+/**
+ * Interface CollectionInterface
+ *
+ * Represents an interface for a collection of items with various utility methods for manipulation and filtering.
+ *
+ * @package Inspira\Collection
+ */
+interface CollectionInterface extends IteratorAggregate, ArrayAccess, Countable, Arrayable
+{
+	/**
+	 * Create a new collection instance.
+	 *
+	 * @param array $data The initial data for the collection.
+	 *
+	 * @return static The new collection instance.
+	 */
+	public static function make(array $data): static;
+
+	/**
+	 * Get the first item in the collection.
+	 *
+	 * @return mixed The first item.
+	 */
+	public function first(): mixed;
+
+	/**
+	 * Get the last item in the collection.
+	 *
+	 * @return mixed The last item.
+	 */
+	public function last(): mixed;
+
+	/**
+	 * Get an item at a specific index.
+	 *
+	 * @param int $index The index of the item.
+	 * @param bool $strict Whether to throw an exception if the index is out of bounds.
+	 *
+	 * @return mixed|null The item at the specified index.
+	 */
+	public function index(int $index, bool $strict = true): mixed;
+
+	/**
+	 * Get the values of a given column.
+	 *
+	 * @param string $column The name of the column.
+	 * @param ?string $key The name of the key column.
+	 *
+	 * @return static A new collection with the values of the specified column.
+	 */
+	public function column(string $column, string $key = null): static;
+
+	/**
+	 * Chunk the collection into smaller chunks.
+	 *
+	 * @param int $length The size of each chunk.
+	 * @param bool $preserveKeys Whether to preserve the keys of the original collection.
+	 *
+	 * @return static A new collection with the chunked data.
+	 */
+	public function chunk(int $length, bool $preserveKeys = false): static;
+
+	/**
+	 * Combine the given keys to the current collection item
+	 *
+	 * @param array|Traversable $keys The keys to use.
+	 *
+	 * @return static A new collection with the specified keys.
+	 */
+	public function combine(array|Traversable $keys): static;
+
+	/**
+	 * Determine if the collection has a given item.
+	 *
+	 * @param mixed $item The item to search for.
+	 *
+	 * @return bool True if the collection has the specified item, false otherwise.
+	 */
+	public function has(mixed $item): bool;
+
+	/**
+	 * Get the keys of the collection items.
+	 *
+	 * @return static A new collection with the keys of the original collection.
+	 */
+	public function keys(): static;
+
+	/**
+	 * Get the values of the collection items.
+	 *
+	 * @return static A new collection with the values of the original collection.
+	 */
+	public function values(): static;
+
+	/**
+	 * Append an item to the end of the collection.
+	 *
+	 * @param mixed $item The item to append.
+	 *
+	 * @return static The modified collection.
+	 */
+	public function append(mixed $item): static;
+
+	/**
+	 * Prepend an item to the beginning of the collection.
+	 *
+	 * @param mixed $item The item to prepend.
+	 *
+	 * @return static A new collection with the prepended value.
+	 */
+	public function prepend(mixed $item): static;
+
+	/**
+	 * Remove an item from the collection by key.
+	 *
+	 * @param int|string $key The key of the item to remove.
+	 *
+	 * @return static A new collection with the specified item removed.
+	 */
+	public function unset(int|string $key): static;
+
+	/**
+	 * Filter the collection items based on the given filters.
+	 *
+	 * @param string $column The column to search by.
+	 * @param mixed $comparison The comparison operator to apply.
+	 * @param mixed $value The value to search for.
+	 * @return static A new collection with the filtered items.
+	 */
+	public function where(string $column, mixed $comparison, mixed $value): static;
+
+	/**
+	 * Filter the collection items based on the given callback.
+	 *
+	 * @param callable|Closure $callback The callback that implements how the collection is filtered.
+	 * @return static A new collection with the filtered items.
+	 */
+	public function filter(callable|Closure $callback): static;
+
+	/**
+	 * Dynamically retrieve the value of an item in the collection.
+	 *
+	 * @param string $name The name of the item.
+	 *
+	 * @return mixed The value of the specified item.
+	 */
+	public function __get(string $name): mixed;
+}
--- GenericCollection.php
+++ GenericCollection.php
@@ -4,28 +4,32 @@ declare(strict_types=1);
 
 namespace Inspira\Collection;
 
-use ArrayAccess;
 use ArrayIterator;
-use Countable;
+use Closure;
+use Inspira\Collection\Contracts\CollectionInterface;
 use Inspira\Collection\Enums\Type;
 use Inspira\Collection\Exceptions\InvalidLiteralTypeException;
 use Inspira\Collection\Exceptions\InvalidTypeException;
 use Inspira\Collection\Exceptions\ItemNotFoundException;
-use Inspira\Contracts\Arrayable;
-use IteratorAggregate;
 use Traversable;
 
 /**
  * @template T The type of collection item.
  */
-class GenericCollection implements IteratorAggregate, ArrayAccess, Countable, Arrayable
+class GenericCollection implements CollectionInterface
 {
 	/**
 	 * @param array<string|integer, T>|T[] $items
 	 * @param T|Type|string|int|float|bool|null $type
 	 * @param bool $isLiteralType
+	 * @param bool $isMutable
 	 */
-	public function __construct(protected array $items = [], protected Type|string|int|float|bool|null $type = Type::MIXED, protected bool $isLiteralType = false)
+	public function __construct(
+		protected array $items = [],
+		protected Type|string|int|float|bool|null $type = Type::MIXED,
+		protected bool $isLiteralType = false,
+		protected bool $isMutable = false
+	)
 	{
 		$this->validateType();
 	}
@@ -40,7 +44,7 @@ class GenericCollection implements IteratorAggregate, ArrayAccess, Countable, Ar
 		foreach ($this->items as $key => $item) {
 			$actualType = $this->getItemType($item);
 
-			if ($expectedType === $actualType) {
+			if ($this->isValidType($item)) {
 				continue;
 			}
 
@@ -51,6 +55,14 @@ class GenericCollection implements IteratorAggregate, ArrayAccess, Countable, Ar
 
 			throw new InvalidTypeException("Invalid item type encountered at position [$key]. Expecting type [$expectedType], [$actualType] given.");
 		}
+	}
+
+	protected function isValidType(mixed $item): bool
+	{
+		$actualType = $this->getItemType($item);
+		$expectedType = $this->getType();
+
+		return $expectedType === $actualType;
 	}
 
 	protected function getItemType(mixed $item)
@@ -72,10 +84,10 @@ class GenericCollection implements IteratorAggregate, ArrayAccess, Countable, Ar
 	 * This is to differentiate the value between a null item and non-existing item
 	 *
 	 * @param string $name The name of the collection item.
-	 * @return mixed
+	 * @return T|mixed
 	 * @throws ItemNotFoundException
 	 */
-	public function __get(string $name)
+	public function __get(string $name): mixed
 	{
 		if (isset($this->items[$name])) {
 			return $this->items[$name];
@@ -145,5 +157,182 @@ class GenericCollection implements IteratorAggregate, ArrayAccess, Countable, Ar
 	public function first(): mixed
 	{
 		return reset($this->items) ?: null;
+	}
+
+	public static function make(array $data): static
+	{
+		return new static($data);
+	}
+
+	/**
+	 * Get the last item in the collection or return null
+	 *
+	 * @return T|mixed
+	 */
+	public function last(): mixed
+	{
+		return end($this->items) ?: null;
+	}
+
+	/**
+	 * @param int $index
+	 * @param bool $strict
+	 * @return T|mixed
+	 */
+	public function index(int $index, bool $strict = false): mixed
+	{
+		if (!$this->offsetExists($index) && $strict === true) {
+			throw new ItemNotFoundException("Item at position [$index] does not exist in the collection.");
+		}
+
+		return $this->offsetGet($index);
+	}
+
+	public function column(string|int $column, string $key = null): static
+	{
+		$items = array_column($this->items, $column, $key);
+
+		return new static($items);
+	}
+
+	public function chunk(int $length, bool $preserveKeys = false): static
+	{
+		$items = array_chunk($this->items, $length, $preserveKeys);
+
+		return new static($items, Type::ARRAY);
+	}
+
+	/**
+	 * @param Traversable|array $keys
+	 * @return static
+	 */
+	public function combine(Traversable|array $keys): static
+	{
+		$keys = is_array($keys) ? $keys : iterator_to_array($keys);
+		$items = $this->items;
+		$items = array_combine($keys, $items);
+
+		if ($this->isMutable) {
+			$this->items = $items;
+			return $this;
+		}
+
+		return new static($items, $this->type, $this->isLiteralType, $this->isMutable);
+	}
+
+	/**
+	 * @inheritdoc
+	 * @param T|mixed $item
+	 * @return bool
+	 */
+	public function has(mixed $item): bool
+	{
+		return in_array($item, $this->items);
+	}
+
+	/**
+	 * @inheritdoc
+	 * @return static
+	 */
+	public function keys(): static
+	{
+		return new static(array_keys($this->items));
+	}
+
+	/**
+	 * @inheritdoc
+	 * @return static
+	 */
+	public function values(): static
+	{
+		return new static(array_values($this->items));
+	}
+
+	/**
+	 * @inheritdoc
+	 * @param T|mixed $item
+	 * @return static
+	 */
+	public function append(mixed $item): static
+	{
+		$expectedType = $this->getType();
+		if (!$this->isValidType($item)) {
+			$actualType = $this->getItemType($item);
+			$actualType = is_object($actualType) ? get_class($actualType) : $actualType;
+			if ($this->isLiteralType) {
+				throw new InvalidLiteralTypeException("Invalid item type encountered during append. Expecting literal [$expectedType], [$actualType] given.");
+			}
+
+			throw new InvalidTypeException("Invalid item type encountered during append. Expecting type [$expectedType], [$actualType] given.");
+		}
+
+		if ($this->isMutable) {
+			$this->offsetSet(null, $item);
+
+			return $this;
+		}
+
+		$items = $this->items;
+		$items[] = $item;
+		return new static($items, $this->type, $this->isLiteralType, $this->isMutable);
+	}
+
+	/**
+	 * @inheritdoc
+	 * @param T|mixed $item
+	 * @return static
+	 */
+	public function prepend(mixed $item): static
+	{
+		$expectedType = $this->getType();
+		if (!$this->isValidType($item)) {
+			$actualType = $this->getItemType($item);
+			$actualType = is_object($actualType) ? get_class($actualType) : $actualType;
+			if ($this->isLiteralType) {
+				throw new InvalidLiteralTypeException("Invalid item type encountered during preprend. Expecting literal [$expectedType], [$actualType] given.");
+			}
+
+			throw new InvalidTypeException("Invalid item type encountered during preprend. Expecting type [$expectedType], [$actualType] given.");
+		}
+
+		if ($this->isMutable) {
+			array_unshift($this->items, $item);
+
+			return $this;
+		}
+
+		$items = $this->items;
+		array_unshift($items, $item);
+
+		return new static($items, $this->type, $this->isLiteralType, $this->isMutable);
+	}
+
+	/**
+	 * @inheritdoc
+	 * @param int|string $key
+	 * @return static
+	 */
+	public function unset(int|string $key): static
+	{
+		if ($this->isMutable) {
+			$this->offsetUnset($key);
+
+			return $this;
+		}
+
+		$items = $this->items;
+		unset($items[$key]);
+
+		return new static($items, $this->type, $this->isLiteralType, $this->isMutable);
+	}
+
+	public function where(string $column, mixed $comparison, mixed $value): static
+	{
+		// TODO: Implement where() method.
+	}
+
+	public function filter(callable|Closure $callback): static
+	{
+		// TODO: Implement filter() method.
 	}
 }
